fc3a45e16c1c4e81faad81959f3477dcd3582019
huyenngk <huyen.nguyen@kellogg.northwestern.edu>
2020-03-05 00:58:17 +0000

    class_3_4_2020

--- app.rb
+++ app.rb
@@ -45,7 +45,16 @@ end
 
 get "/events/:id/rsvps/create" do
     puts "params: #{params}"
-
+    # first find the event that rsvp'ing for 
+    @event = events_table.where(id: params[:id]).to_a[0]
+    # insert a row in the rsvp table 
+    rsvps_table.insert(
+        event_id: @event[:id],
+        name: params["name"],
+        email: params["email"],
+        comments: params["comments"],
+        going: params["going"], 
+    )
     view "create_rsvp"
 end
 
